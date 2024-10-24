34ae4e74f94ae7b321da65c51a815c50d7045bf9
YoumiMa <youmima1015@gmail.com>
2024-10-24 23:18:34 +0900

    eos

--- train_llm.py
+++ train_llm.py
@@ -14,8 +14,7 @@ from transformers import AutoModelForCausalLM, BitsAndBytesConfig
 set_seed(42)
 
 # Hugging Face Hub上のリポジトリからデータセットを読み込む
-dataset = load_dataset("tokyotech-llm/Swallow-Instruct-v0.1", split="train")
-# データセットの形式と事例数を確認します。
+dataset = load_dataset("json", data_files="/gs/fs/tga-okazaki/ma/lmsys-chat-1m/sft/lmsys-chat-1m-synth-sft.jsonl.gz", split="train")
 
 base_model_name = "tokyotech-llm/Llama-3.1-Swallow-8B-v0.1"
 tokenizer_name = "tokyotech-llm/Llama-3.1-Swallow-8B-Instruct-v0.1"
@@ -23,75 +22,56 @@ tokenizer_name = "tokyotech-llm/Llama-3.1-Swallow-8B-Instruct-v0.1"
 tokenizer = AutoTokenizer.from_pretrained(tokenizer_name)
 tokenized_dataset = [
     tokenizer.apply_chat_template(item["conversation"])
-    for item in dataset
+    for item in dataset.select(range(1000))
 ]
 
-tokenizer.pad_token = tokenizer.unk_token
+instruction_ids = tokenizer.encode("<|start_header_id|>user<|end_header_id|>\n\n")[1:] # no begin of text
+response_ids = tokenizer.encode("<|start_header_id|>assistant<|end_header_id|>\n\n")[1:]# no begin of text
 
-
-
-bos = tokenizer.bos_token
 collator = DataCollatorForCompletionOnlyLM(
-    instruction_template=bos + "ユーザ：",  # ユーザの発話開始を示す文字列
-    response_template=bos + "アシスタント：",  # アシスタントの返答開始を示す文字列
+    instruction_template=instruction_ids,  # ユーザの発話開始を示す文字列
+    response_template=response_ids,  # アシスタントの返答開始を示す文字列
     tokenizer=tokenizer,  # トークナイザ
 )
-# トークナイズされたデータセットの先頭をミニバッチ構築処理
-batch = collator(tokenized_dataset[:1])
-input_ids = batch["input_ids"][0]
-labels = batch["labels"][0]
-print("入力トークンID:", input_ids)
-print("正解ラベル:", labels)
-
-segments_to_fit: list[list[int]] = []
-segments_to_ignore: list[list[int]] = []
-# ラベルが-100である箇所とそうでない箇所ごとにグルーピング
-for key, group in itertools.groupby(
-    range(len(input_ids)), key=lambda i: labels[i] == -100
-):
-    group = list(group)
-    if key:
-        segments_to_ignore.append(group)
-    else:
-        segments_to_fit.append(group)
-
-print("---- 損失を計算しない部分 ----")
-for seg in segments_to_ignore:
-    print(tokenizer.decode(input_ids[seg]))
-    print()
-
-print("---- 損失を計算する部分 ----")
-for seg in segments_to_fit:
-    print(tokenizer.decode(input_ids[seg]))
-    print()
 
 
 model = AutoModelForCausalLM.from_pretrained(
     base_model_name,
-    torch_dtype=torch.bfloat16,
+#    torch_dtype=torch.bfloat16,
     use_cache=False,  # 後にgradient checkpointingを有効にするために必要
     device_map="auto",
 )
      
-     
+#model.config.eos_token_id = [
+#    128001,
+#    128008,
+#    128009
+#  ]
+
+#model.generation_config.eos_token_id = [
+#    128001,
+#    128008,
+#    128009]
+
 # 学習パラメータ
 training_args = TrainingArguments(
     output_dir="/gs/bs/tga-okazaki/ma/results", # 結果の保存フォルダ
-    bf16=True,  # BF16を使用した学習の有効化
+    bf16=False,
+    fp16=True, 
     num_train_epochs=1,  # エポック数
     per_device_train_batch_size=2,  # 訓練時のバッチサイズ
-    gradient_accumulation_steps=8,  # 勾配累積のステップ数（5.5.2）
+    gradient_accumulation_steps=64,  # 勾配累積のステップ数（5.5.2）
     gradient_checkpointing=True,  # 勾配チェックポインティングの有効化（5.5.3）
     optim="paged_adamw_8bit",  # 最適化器
-    learning_rate=3e-4,  # 学習率
+    learning_rate=1e-5,  # 学習率
     lr_scheduler_type="cosine",  # 学習率スケジューラの種類
-    max_grad_norm=0.3,  # 勾配クリッピングにおけるノルムの最大値（9.4.3）
+    max_grad_norm=0.5,  # 勾配クリッピングにおけるノルムの最大値（9.4.3）
     warmup_ratio=0.1,  # 学習率のウォームアップの長さ（5.2.8）
     logging_steps=10,  # ロギングの頻度
     save_steps=300,  # モデルの保存頻度
 )
 
-tokenized_dataset = tokenized_dataset[:1000]
+tokenized_dataset = tokenized_dataset[:50000]
 
 trainer = Trainer(
     model,
@@ -102,3 +82,21 @@ trainer = Trainer(
 )
 
 trainer.train()
+
+
+print("Saving model")
+trainer.save_model()
+
+prompt = dataset[1]["conversation"][0]["content"]
+messages = [{"role": "user", "content": prompt}]
+tokenized_chat = tokenizer.apply_chat_template(
+    messages,
+    tokenize=True,
+    add_generation_prompt=True,
+    return_tensors="pt"
+)
+generated_tokens = model.generate(tokenized_chat, max_new_tokens=2048)
+generated_text = tokenizer.decode(generated_tokens[0])
+print(generated_text)
+print("====")
+print(len(generated_tokens[0]),generated_tokens[0])
