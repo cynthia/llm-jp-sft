d718020643cb332a1fdfc4070aca61c06acf7b92
YoumiMa <youmima1015@gmail.com>
2024-11-05 12:15:44 +0900

    using instruct model tokenizer

--- train_llm.py
+++ train_llm.py
@@ -102,7 +102,7 @@ def main():
     training_args, sft_training_args = parser.parse_args_into_dataclasses()    
     
     tokenizer_name_or_path: str = (
-        sft_training_args.tokenizer_name_or_path
+        sft_training_args.tokenizer_name_or_path or sft_training_args.model_name_or_path
     )
     logger.info(f"Loading tokenizer from {tokenizer_name_or_path}")
     
@@ -113,21 +113,6 @@ def main():
         trust_remote_code=True,
     )
 
-    chat_template = tokenizer.chat_template
-    eos_token = tokenizer.eos_token
-    pad_token = tokenizer.pad_token
-
-    tokenizer = AutoTokenizer.from_pretrained(
-        sft_training_args.model_name_or_path,
-        use_fast=sft_training_args.use_fast,
-        additional_special_tokens=sft_training_args.additional_special_tokens,
-        trust_remote_code=True,
-    )
-    
-    tokenizer.chat_template = chat_template
-    tokenizer.eos_token = eos_token
-    tokenizer.pad_token = pad_token
-    
     print(tokenizer.special_tokens_map, "ids:", tokenizer.all_special_ids)
     logger.info("Loading data")
     
